a4a70cf961d38adbbca7c00e2640de24d30f8784
EP <ep@github.com>
2019-08-09 17:14:51 -0400

    Initial out subtree commit

---
+++ out/_next/static/webpack/static/development/pages/index.js.264d815ed9c741de8fa6.hot-update.js
@@ -0,0 +1,26 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./assets/images/reactide-demo3.png":
+/*!******************************************!*\
+  !*** ./assets/images/reactide-demo3.png ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/reactide-demo3-c333b1afbc5859c990ac9a71f24ca737.png";
+
+/***/ }),
+
+/***/ "./assets/images/reactide-demo4.png":
+/*!******************************************!*\
+  !*** ./assets/images/reactide-demo4.png ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "/_next/static/images/reactide-demo4-45c2ba5c2610716aabe069068d5f3916.png";
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.264d815ed9c741de8fa6.hot-update.js.map
